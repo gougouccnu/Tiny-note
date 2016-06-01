6c78d1682e2485bea3cea1c0f874aeac63d7b7ed
gougouccnu <lishaoweiccnu@163.com>
2016-06-01 11:53:20 +0800

    修改默认笔记时间

--- app/src/main/java/com/mycompany/tinynote/db/NoteOpenHelper.java
+++ app/src/main/java/com/mycompany/tinynote/db/NoteOpenHelper.java
@@ -50,9 +50,9 @@ public class NoteOpenHelper extends SQLiteOpenHelper {
                 "month: " + month + "day: " + day);
         db.execSQL(CREATE_NOTE);
         db.execSQL("insert into Note (year, month, title, content, location, date) values (?,?,?,?,?,?)",
-                new String[] {year, month, "要有光", "上帝说\n要有光\n于是\n便有了光 ", "湖北武汉", day});
+                new String[] {year, month, "要有光", "上帝说\n要有光\n于是\n便有了光 ", "湖北武汉", year + month + day});
         db.execSQL("insert into Note (year, month, title, content, location, date) values (?,?,?,?,?,?)",
-                new String[] {year, month, "季风气候", "没道理\n是一枚太平洋的暖湿空气\n飘散了我们的心\n在青春的墓地\n就这样平行下去", "于武汉", day});
+                new String[] {year, month, "季风气候", "没道理\n是一枚太平洋的暖湿空气\n飘散了我们的心\n在青春的墓地\n就这样平行下去", "于武汉", year + month +day});
         Log.d("MyDatabaseHelper", "mydatabase created");
     }
 
